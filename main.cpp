c50346ec4b548c272df11f571ba3fdc13ec7fe42
Vebnik(Nik) <vebnik111@gmail.com>
2022-08-18 21:09:30 +0300

    add math headers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include<iostream>
+#include <iostream>
+#include "math.h"
 
 int main()
 {
-	std::cout << "hello nice guy" << std::endl;
+
+	std::cout << factorial(5) << std::endl;
+
+	std::cout << sum(5, 5) << std::endl;
 
 	return 1;
 }
---
+++ math.h
@@ -0,0 +1,13 @@
+int factorial(int num) {
+
+  int tmp = 1;
+
+  for (int i = 1; i <= num; i++)
+    tmp = i * tmp;
+  
+  return tmp;
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
